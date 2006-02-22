0afc6e8a1d9d40c2c625cef6280b8244608a0de2
bertrik <bertrik@79f01b3a-5824-0410-bb8e-e6c70fe0d4c3>
2006-02-22 22:15:46 +0000

    cleanup
    git-svn-id: https://lpcusb.svn.sourceforge.net/svnroot/lpcusb/trunk@4 79f01b3a-5824-0410-bb8e-e6c70fe0d4c3

--- target/main.c
+++ target/main.c
@@ -25,7 +25,7 @@
 #include "startup.h"
 
 
-
+#define BAUD_RATE	115200
 
 #define BULK_OUT_EP		0x05
 #define BULK_IN_EP		0x82
@@ -35,67 +35,69 @@
 #define LE_WORD(x)		(x&0xFF),(x>>8)
 
 
+static U8 abBulkBuf[64];
+
 static const U8 abDescriptors[] = {
 
 // device descriptor	
 	0x12,
-	DESC_DEVICE,
-	LE_WORD(0x0110),
-	0x00,
-	0x00,
-	0x00,
-	MAX_PACKET_SIZE,		// max packet size
-	LE_WORD(0x1234),
-	LE_WORD(0xABCD),
-	LE_WORD(0x0100),
-	0x01,
-	0x02,
-	0x03,
-	0x01,
+	DESC_DEVICE,			
+	LE_WORD(0x0110),		// bcdUSB
+	0x00,					// bDeviceClass
+	0x00,					// bDeviceSubClass
+	0x00,					// bDeviceProtocol
+	MAX_PACKET_SIZE,		// bMaxPacketSize
+	LE_WORD(0x1234),		// idVendor
+	LE_WORD(0xABCD),		// idProduct
+	LE_WORD(0x0100),		// bcdDevice
+	0x01,					// iManufacturer
+	0x02,					// iProduct
+	0x03,					// iSerialNumber
+	0x01,					// bNumConfigurations
 
 // configuration descriptor
 	0x09,
 	DESC_CONFIGURATION,
-	LE_WORD(0x20),	// wLength
-	0x01,			// 1 interface
-	0x01,			// config value
-	0x00,			// string descriptor
-	0xC0,			// attributes: self-powered
-	0x32,			// maxpower
+	LE_WORD(0x20),			// wTotalLength
+	0x01,					// bNumInterfaces
+	0x01,					// bConfigurationValue
+	0x00,					// iConfiguration
+	0xC0,					// bmAttributes
+	0x32,					// bMaxPower
 
 // interface
 	0x09,
 	DESC_INTERFACE,
-	0x00,		// interface #0
-	0x00,		// alternate
-	0x02,		// endpoints
-	0x08,		// class code = MSC
-	0x06,		// subclass = SCSI
-	0x50,		// protocol = BOT
-	0x00,		// string index
+	0x00,					// bInterfaceNumber
+	0x00,					// bAlternateSetting
+	0x02,					// bNumEndPoints
+	0x08,					// bInterfaceClass
+	0x06,					// bInterfaceSubClass
+	0x50,					// bInterfaceProtocol
+	0x00,					// iInterface
 // EP
 	0x07,
 	DESC_ENDPOINT,
-	BULK_IN_EP,		// EP address = in
-	0x02,			// bmAttributes = bulk
-	LE_WORD(0x40),		// max packet size
-	0x00,			// interval
+	BULK_IN_EP,				// bEndpointAddress
+	0x02,					// bmAttributes = bulk
+	LE_WORD(0x40),			// wMaxPacketSize
+	0x00,					// bInterval
 // EP
 	0x07,
 	DESC_ENDPOINT,
-	BULK_OUT_EP,
-	0x02,			// bmAttributes = bulk
-	LE_WORD(0x40),	// max packet size
-	0x00,			// interval
+	BULK_OUT_EP,			// bEndpointAddress
+	0x02,					// bmAttributes = bulk
+	LE_WORD(0x40),			// wMaxPacketSize
+	0x00,					// bInterval
 
 // string descriptors
 	0x04,
 	DESC_STRING,
 	LE_WORD(0x0409),
 
-	0x1A,
+	0x0E,
 	DESC_STRING,
-	'M', 0, 'a', 0, 'n', 0, 'u', 0, 'f', 0, 'a', 0, 'c', 0, 't', 0, 'u', 0, 'r', 0, 'e', 0, 'r', 0,
+	'L', 0, 'P', 0, 'C', 0, 'U', 0, 'S', 0, 'B', 0,
 
 	0x12,
 	DESC_STRING,
@@ -108,8 +110,6 @@ static const U8 abDescriptors[] = {
 // terminating zero
 	0
 };
-
-static U8 abBulkBuf[64];
 
 
 static void BulkIn(U8 bEP, U8 bEPStatus)
@@ -147,10 +147,6 @@ static BOOL HandleClassRequest(TSetupPacket *pSetup, int *piLen, U8 **ppbData)
 	return TRUE;
 }
 
-
-
-
-#define BAUD_RATE	115200
 
 /*************************************************************************
 	main
