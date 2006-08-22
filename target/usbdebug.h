90e8ac7f962b364af5deedc7efa51ff4f0a1a2d5
bertrik <bertrik@79f01b3a-5824-0410-bb8e-e6c70fe0d4c3>
2006-08-22 19:19:30 +0000

    Updated DBG macro for better portability
    git-svn-id: https://lpcusb.svn.sourceforge.net/svnroot/lpcusb/trunk@89 79f01b3a-5824-0410-bb8e-e6c70fe0d4c3

--- target/usbdebug.h
+++ target/usbdebug.h
@@ -3,10 +3,12 @@
 int printf(const char *format, ...);
 
 #ifdef DEBUG
-#define DBG(x ...)	printf(x)
+//#define DBG(x ...)	printf(x)
+#define DBG	printf
 #define ASSERT(x)	if(!(x)){DBG("\nAssertion '%s' failed in %s:%s#%d!\n",#x,__FILE__,__FUNCTION__,__LINE__);while(1);}
 #else
-#define DBG(x ...)
+//#define DBG(x ...)
+#define DBG
 #define ASSERT(x)
 #endif
 
