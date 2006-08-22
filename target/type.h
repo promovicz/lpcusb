0881791c616b65a1d8aa80c62f672b8189b7ea8d
bertrik <bertrik@79f01b3a-5824-0410-bb8e-e6c70fe0d4c3>
2006-08-22 19:16:47 +0000

    Doxygen updates
    git-svn-id: https://lpcusb.svn.sourceforge.net/svnroot/lpcusb/trunk@87 79f01b3a-5824-0410-bb8e-e6c70fe0d4c3

--- target/type.h
+++ target/type.h
@@ -1,28 +1,30 @@
-/*
+/**
+	@file
+	primitive types used in the USB stack
+	
 	(c) 2006, Bertrik Sikken, bertrik@sikken.nl
-*/
+ */
 
-/* primitive types used in the USB stack */
 
 #ifndef _TYPE_H_
 #define _TYPE_H_
 
-typedef unsigned char		U8;
-typedef unsigned short int	U16;
-typedef unsigned int		U32;
+typedef unsigned char		U8;		/**< unsigned 8-bit */
+typedef unsigned short int	U16;	/**< unsigned 16-bit */
+typedef unsigned int		U32;	/**< unsigned 32-bit */
 
-typedef int					BOOL;
+typedef int					BOOL;	/**< #TRUE or #FALSE */
 
-#define	TRUE	1
-#define FALSE	0
+#define	TRUE	1					/**< TRUE */
+#define FALSE	0					/**< FALSE */
 
 #ifndef NULL
-#define NULL	((void*)0)
+#define NULL	((void*)0)			/**< NULL pointer */
 #endif
 
 /* some other useful macros */
-#define MIN(x,y)	((x)<(y)?(x):(y))
-#define MAX(x,y)	((x)>(y)?(x):(y))
+#define MIN(x,y)	((x)<(y)?(x):(y))	/**< MIN */
+#define MAX(x,y)	((x)>(y)?(x):(y))	/**< MAX */
 
 
 #endif /* _TYPE_H_ */
