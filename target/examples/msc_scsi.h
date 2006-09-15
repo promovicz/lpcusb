29a2b58b647476ea726a298e9b12513617ff30df
bertrik <bertrik@79f01b3a-5824-0410-bb8e-e6c70fe0d4c3>
2006-09-15 14:34:52 +0000

    Update iCDBLen parameter type in SCSIHandleData and SCSIHandleCmd
    git-svn-id: https://lpcusb.svn.sourceforge.net/svnroot/lpcusb/trunk@110 79f01b3a-5824-0410-bb8e-e6c70fe0d4c3

--- target/examples/msc_scsi.h
+++ target/examples/msc_scsi.h
@@ -20,5 +20,5 @@
 #include "type.h"
 
 void	SCSIReset(void);
-U8 *	SCSIHandleCmd(U8 *pbCDB, int iCDBLen, int *piRspLen, BOOL *pfDevIn);
-U8 *	SCSIHandleData(U8 *pbCDB, int iCDBLen, U8 *pbData, U32 dwOffset);
+U8 *	SCSIHandleCmd(U8 *pbCDB, U8 bCDBLen, int *piRspLen, BOOL *pfDevIn);
+U8 *	SCSIHandleData(U8 *pbCDB, U8 bCDBLen, U8 *pbData, U32 dwOffset);
