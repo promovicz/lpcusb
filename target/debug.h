98f8d76a2aff745a301a432b494464601792268c
Ingo Albrecht <prom@berlin.ccc.de>
2011-07-12 06:55:10 +0200

    Various patches to enable building as a library.

--- target/debug.h
+++ target/debug.h
@@ -25,9 +25,6 @@
 	THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
-int printf(const char *format, ...);
-
 #ifdef DEBUG
 #define DBG	printf
 #define ASSERT(x)	if(!(x)){DBG("\nAssertion '%s' failed in %s:%s#%d!\n",#x,__FILE__,__FUNCTION__,__LINE__);while(1);}
